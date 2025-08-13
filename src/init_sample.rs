c066b11a1593bcbdaa6c79c89c549d5d3b95507e
Wholesome100 <77867156+Wholesome100@users.noreply.github.com>
2025-08-13 16:02:35 -0400

    Slot in viewport and orbit code

---
+++ src/init_sample.rs
@@ -0,0 +1,60 @@
+/*
+    I think a fun and doable game would be a sand castle game.
+    Essentially the player controls a small shovel on a small island, with tools to help them build out a sandcastle.
+    At different intervals waves wash over and threaten to destroy their sandcastle.
+    Would be simple enough with a static camera, the biggest hurtle being implementing the particle systems.
+    I need to physically simulate sand and water.
+ */
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_plugins(HelloPlugin)
+    .run();
+}
+
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, (update_people, greet_people).chain());
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    for name in &query {
+        if timer.0.tick(time.delta()).just_finished() {
+            println!("Hello {}!", name.0);
+        }
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Elaina Proctor" {
+            name.0 = "Elaina Huma".to_string();
+            break;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,55 +6,148 @@
     I need to physically simulate sand and water.
  */
 
-use bevy::prelude::*;
+use std::{f32::consts::{FRAC_1_PI, FRAC_PI_2}, ops::Range};
+
+use bevy::{input::mouse::AccumulatedMouseMotion, prelude::*};
+
+#[derive(Debug, Resource)]
+struct CameraSettings {
+    pub orbit_distance: f32,
+    pub pitch_speed: f32,
+    // Clamp pitch to this range
+    pub pitch_range: Range<f32>,
+    pub roll_speed: f32,
+    pub yaw_speed: f32,
+}
+
+impl Default for CameraSettings {
+    fn default() -> Self {
+         let pitch_limit = FRAC_PI_2 - 0.1;
+         Self {
+            orbit_distance: 20.0,
+            pitch_speed: 0.003,
+            // top hemisphere correseponds from -pitch limit to 0, bottom is from 0 to pitch limit
+            pitch_range: -pitch_limit..-0.01,
+            roll_speed: 1.0,
+            yaw_speed: 0.004,
+        }
+    }
+}
 
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins)
-    .add_plugins(HelloPlugin)
-    .run();
+        .add_plugins(DefaultPlugins)
+        .init_resource::<CameraSettings>()
+        .add_systems(Startup, setup)
+        .add_systems(Update, (draw_cursor, orbit))
+        .run();
 }
 
+fn draw_cursor(
+    camera_query: Single<(&Camera, &GlobalTransform)>,
+    mouse_buttons: Res<ButtonInput<MouseButton>>,
+    ground: Single<&GlobalTransform, With<Ground>>,
+    windows: Query<&Window>,
+    mut gizmos: Gizmos,
+) {
+    let Ok(windows) = windows.single() else {
+        return;
+    };
 
-pub struct HelloPlugin;
+    let (camera, camera_transform) = *camera_query;
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
-        app.add_systems(Startup, add_people);
-        app.add_systems(Update, (update_people, greet_people).chain());
+    let Some(cursor_position) = windows.cursor_position() else {
+        return;
+    };
+
+    // Calculate a ray pointing from the camera into the world based on the cursor's position.
+    let Ok(ray) = camera.viewport_to_world(camera_transform, cursor_position) else {
+        return;
+    };
+
+    // Calculate if and where the ray is hitting the ground plane.
+    let Some(distance) =
+        ray.intersect_plane(ground.translation(), InfinitePlane3d::new(ground.up()))
+    else {
+        return;
+    };
+    let point = ray.get_point(distance);
+
+    // Draw a circle just above the ground plane at that position.
+    gizmos.circle(
+        Isometry3d::new(
+            point + ground.up() * 0.01,
+            Quat::from_rotation_arc(Vec3::Z, ground.up().as_vec3()),
+        ),
+        0.2,
+        Color::WHITE,
+    );
+}
+
+fn orbit(
+    mut camera: Single<&mut Transform, With<Camera>>,
+    camera_settings: Res<CameraSettings>,
+    mouse_buttons: Res<ButtonInput<MouseButton>>,
+    mouse_motion: Res<AccumulatedMouseMotion>,
+    time: Res<Time>,
+) {
+    let delta = mouse_motion.delta;
+    let mut delta_roll = 0.0;
+
+   
+    if mouse_buttons.pressed(MouseButton::Right) {
+        // Mouse motion is one of the few inputs that should not be multiplied by delta time,
+        // as we are already receiving the full movement since the last frame was rendered. Multiplying
+        // by delta time here would make the movement slower that it should be.
+        let delta_pitch = delta.y * camera_settings.pitch_speed;
+        let delta_yaw = delta.x * camera_settings.yaw_speed;
+
+        // Conversely, we DO need to factor in delta time for mouse button inputs.
+        delta_roll *= camera_settings.roll_speed * time.delta_secs();
+
+        // Obtain the existing pitch, yaw, and roll values from the transform.
+        let (yaw, pitch, roll) = camera.rotation.to_euler(EulerRot::YXZ);
+
+        // Establish the new yaw and pitch, preventing the pitch value from exceeding our limits.
+        let pitch = (pitch + delta_pitch).clamp(
+            camera_settings.pitch_range.start,
+            camera_settings.pitch_range.end,
+        );
+        let roll = roll + delta_roll;
+        let yaw = yaw + delta_yaw;
+        camera.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+
+        // Adjust the translation to maintain the correct orientation toward the orbit target.
+        // In our example it's a static target, but this could easily be customized.
+        let target = Vec3::ZERO;
+        camera.translation = target - camera.forward() * camera_settings.orbit_distance;
     }
 }
 
 #[derive(Component)]
-struct Person;
+struct Ground;
 
-#[derive(Component)]
-struct Name(String);
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(20., 20.))),
+        MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
+        Ground,
+    ));
 
-#[derive(Resource)]
-struct GreetTimer(Timer);
+    // light
+    commands.spawn((
+        DirectionalLight::default(),
+        Transform::from_translation(Vec3::ONE).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    for name in &query {
-        if timer.0.tick(time.delta()).just_finished() {
-            println!("Hello {}!", name.0);
-        }
-    }
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Huma".to_string();
-            break;
-        }
-    }
+    // camera
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(15.0, 5.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 }
