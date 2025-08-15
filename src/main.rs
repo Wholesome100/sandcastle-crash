79f8fd46492f489c5105e92bbbb55dfd98fde981
Wholesome100 <77867156+Wholesome100@users.noreply.github.com>
2025-08-15 12:14:09 -0400

    Refactor into 2D game

--- src/main.rs
+++ src/main.rs
@@ -6,43 +6,45 @@
     I need to physically simulate sand and water.
  */
 
-use bevy::{prelude::*};
-
-mod game_camera;
-
-use crate::game_camera::{CameraPlugin, Ground};
-
+use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(CameraPlugin)
         .add_systems(Startup, setup)
+        .add_systems(Update, sprite_movement)
         .run();
 }
 
+#[derive(Component)]
+enum Direction {
+    Left,
+    Right,
+}
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // plane
-    commands.spawn((
-        Mesh3d(meshes.add(Plane3d::default().mesh().size(20., 20.))),
-        MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
-        Ground,
-    ));
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
 
-    // light
     commands.spawn((
-        DirectionalLight::default(),
-        Transform::from_translation(Vec3::ONE).looking_at(Vec3::ZERO, Vec3::Y),
+        Sprite::from_image(asset_server.load("wood_rustic_beam.png")),
+        Transform::from_xyz(0., 0., 0.),
+        Direction::Right,
     ));
+}
 
-    // camera
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(15.0, 5.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
-    ));
+/// The sprite is animated by changing its translation depending on the time that has passed since
+/// the last frame.
+fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
+    for (mut logo, mut transform) in &mut sprite_position {
+        match *logo {
+            Direction::Right => transform.translation.x += 150. * time.delta_secs(),
+            Direction::Left => transform.translation.x -= 150. * time.delta_secs(),
+        }
+
+        if transform.translation.x > 200. {
+            *logo = Direction::Left;
+        } else if transform.translation.x < -200. {
+            *logo = Direction::Right;
+        }
+    }
 }
